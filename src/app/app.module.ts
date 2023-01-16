5331620a640e7e91c03998ccf3d217e645b452f4
Shinde kudasai ❤〜 <changlovemom@gmail.com>
2023-01-16 14:09:12 +0700

    feat: services and add-book

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -17,7 +18,7 @@ import { ViewBookComponent } from './components/view-book/view-book.component';
     ViewBookComponent,
     NavbarComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
