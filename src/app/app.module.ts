bf7d63374bffbc7a64023c9980290b48e0df3f1a
teerapop67 <52346089+teerapop67@users.noreply.github.com>
2023-01-16 12:42:23 +0700

    fixed: change edit's name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,21 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddBookComponent } from './components/add-book/add-book.component';
-import { EditComponent } from './components/edit/edit.component';
+import { EditBookComponent } from './components/edit-book/edit-book.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AddBookComponent
-  ],
+  declarations: [AppComponent, AddBookComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
       {
         path: '',
-        component: EditComponent
-      }
-    ])
+        component: EditBookComponent,
+      },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
