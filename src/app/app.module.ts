7e5f749ffc7a64d03990b55d1184f4d48a78322a
Shinde kudasai ❤〜 <changlovemom@gmail.com>
2023-01-16 12:45:25 +0700

    feat: update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,37 +5,27 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddBookComponent } from './components/add-book/add-book.component';
-<<<<<<< HEAD
-import { EditComponent } from './components/edit/edit.component';
+
+import { EditBookComponent } from './components/edit-book/edit-book.component';
 import { ViewBookComponent } from './components/view-book/view-book.component';
 
 @NgModule({
   declarations: [AppComponent, AddBookComponent, ViewBookComponent],
-=======
-import { EditBookComponent } from './components/edit-book/edit-book.component';
-
-@NgModule({
-  declarations: [AppComponent, AddBookComponent],
->>>>>>> bf7d63374bffbc7a64023c9980290b48e0df3f1a
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
       {
         path: '',
-<<<<<<< HEAD
         component: ViewBookComponent,
       },
       {
         path: '/edit-book',
-        component: EditComponent,
+        component: EditBookComponent,
       },
       {
         path: '/add-book',
         component: AddBookComponent,
-=======
-        component: EditBookComponent,
->>>>>>> bf7d63374bffbc7a64023c9980290b48e0df3f1a
       },
     ]),
   ],
