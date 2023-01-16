af9f68c119abf672d4abac7ab3361670398ea097
Shinde kudasai ❤〜 <changlovemom@gmail.com>
2023-01-16 13:51:02 +0700

    feat: service

---
+++ src/app/service/crud.service.ts
@@ -0,0 +1,82 @@
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, map, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Book {
+  _id!: String;
+  name!: String;
+  price!: String;
+  description!: String;
+}
+
+export class CrudService {
+  // Node.js API
+  REST_API = 'http://locahost:8000/api';
+
+  // http header
+  httpHeaders = new HttpHeaders().set('Content-Type', 'Application/json');
+
+  constructor(private httpClient: HttpClient) {}
+
+  // Add new book
+  AddBook(data: Book) {
+    let API_URL = `${this.REST_API}/add-book`;
+    return this.httpClient
+      .post(API_URL, data)
+      .pipe(catchError(this.handleError));
+  }
+
+  // Get all book(s)
+  GetBooks() {
+    return this.httpClient.get(`${this.REST_API}`);
+  }
+
+  // Get a book
+  GetBook(id: any) {
+    let API_URL = `${this.REST_API}/read-book/${id}`;
+
+    return this.httpClient.get(API_URL, { headers: this.httpHeaders }).pipe(
+      map((res: any) => {
+        return res || {};
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  // Update book
+  UpdateBook(id: any, data: any) {
+    let API_URL = `${this.REST_API}/update-book/${id}`;
+    return this.httpClient
+      .put(API_URL, data, { headers: this.httpHeaders })
+      .pipe(catchError(this.handleError));
+  }
+
+  // Delete
+  DeleteBook(id: any) {
+    let API_URL = `${this.REST_API}/delete-book/${id}`;
+    return this.httpClient
+      .delete(API_URL, { headers: this.httpHeaders })
+      .pipe(catchError(this.handleError));
+  }
+
+  // Handle error
+  handleError(error: HttpErrorResponse) {
+    let errorMsg = '';
+    if (error.error instanceof ErrorEvent) {
+      // Handle client error
+      errorMsg = error.error.message;
+      // Handle server error
+      errorMsg = `Error code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMsg);
+
+    return throwError(errorMsg);
+  }
+}
