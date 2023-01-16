617a4ce7f12e6a4cc38933dd652abc2e69507d15
Mrboatiot <52354253+boatmessi02@users.noreply.github.com>
2023-01-16 12:35:16 +0700

    feat:navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewBooksComponent } from './components/view-books/view-books.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AddBookComponent } from './components/add-book/add-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewBooksComponent,
+    NavbarComponent,
+    AddBookComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
